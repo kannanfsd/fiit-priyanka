f57749c54b8d522ad4d5530b5d91a0b88e5f8f05
Kannan Rajendran <kannannglabs@gmail.com>
2025-05-18 12:40:45 +0530

    modify the welcome.java file

--- Welcome.java
+++ Welcome.java
@@ -5,13 +5,12 @@ public class Welcome {
   }
 }
 
-public - define the accessbility
-class - indicate it is class file
+public 	- define the accessbility
+class 	- indicate it is class file
 Welcome - name of the class 
-{ - opening curly brace } - closed curly brace
-static - it allows to access this method without creating an object
-void - return type
-main - name of the method. [pre-defined one]
+{ 	- opening curly brace 
+} 	- closed curly brace
+static 	- it allows to access this method without creating an object
+void 	- return type
+main 	- name of the method. [pre-defined one]
 System.out.println("Hello");	//statement;
-
-338449
